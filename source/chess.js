36c45f09eb9167f875462d24c5685306178dd76e
Alexosis <alex_mol1999@mail.ru>
2019-09-02 16:29:11 +0300

    Нашёл более подходящую функцию, изменил функцию на стерлочную, добавил JSDoc

--- source/chess.js
+++ source/chess.js
@@ -1,40 +1,20 @@
 'use strict';
 
- const chess = function(N){
+/*
+* Изображает ASCII шахматную доску, размером N*N
+*
+* @param {number} N - сторона доски
+* @return {string} ASCII изображение шахматной доски
+* */
 
-    if(+N<2 || isNaN(+N) || !isFinite(N)){          //проверка на валидность данных
+ const chess = N => {
+
+    if(+N<2 || isNaN(+N) || !isFinite(N)){
         return null;
     }
-    let parity = true;
-    let str1='', str2='';
-    for (let i = 0; i < +N; i++){
-        if(parity){
-            str1 += '*';                            //создание шаблонов двух строк:
-            str2 += ' ';                            //чётной и не четной
-            parity = !parity;
-        }
-        else {
-            str1 += ' ';
-            str2 += '*';
-            parity = !parity;
-        }
 
-    }
-    str1 += '\n';
-    str2 += '\n';
+    const str1 = "* ".repeat(N/2+1).slice(0, N) + '\n';
+    const str2 = " *".repeat(N/2+1).slice(0, N) + '\n';
 
-    parity = true;
-
-    let desk = str1 + str2;
-    for(let i=0; i<+N-2; i++){
-        if(parity){                                 //Поочерёдное присоединение готовых строк
-            desk += str1;
-            parity = !parity;
-        }
-        else{
-            desk += str2;
-            parity = !parity;
-        }
-    }
-    return desk;
+     return (str1 + str2).repeat(N/2+1).slice( 0, str1.length * N);
 };
